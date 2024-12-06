daa3ae3a3f621c0da04adfcf2efe6d53ce6f259a
Philip Andersson <philip.andersson.1991@hotmail.com>
2024-12-07 00:14:58 +0100

    grattis på födelsedagen gårkan pojken, saknar dig <3

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -17,23 +17,3 @@ class Job extends Model
         return $this->belongsTo(Employer::class);
     }
 }
-
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
-
-// class Job extends Model
-// {
-
-//     use HasFactory;
-//     protected $table = 'job_listings';
-
-//     protected $fillable = [
-//         'title',
-//         'salary'
-//     ];
-
-//     public function employer()
-//     {
-//         return $this->belongsTo(Employer::class);
-//     }
-// }
